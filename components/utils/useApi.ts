0ad1cfb2f11dfe14c2d3a71b3bed2fc915f52765
Izzy <93226456+izzetxyz@users.noreply.github.com>
2024-07-23 16:06:28 +0300

    Update useApi.ts

--- components/utils/useApi.ts
+++ components/utils/useApi.ts
@@ -1,4 +1,4 @@
-// src/components/utils/useApi.ts
+
 import { Result } from 'postcss';
 import { CharacterCard,Info } from '../models/types';
 
@@ -6,11 +6,11 @@ export const fetchCharacters = async (): Promise<CharacterCard[]> => {
   const res = await fetch('https://rickandmortyapi.com/api/character/?page=1');
   const json = await res.json();
 
-  return json.results; // API'den gelen kart verileri
+  return json.results; 
 };
 export const fetchInfo = async (): Promise<Info[]> => {
   const res = await fetch('https://rickandmortyapi.com/api/character/?page=1');
   const json = await res.json();
 
-  return json.info; // API'den gelen kart verileri
+  return json.info;
 };
